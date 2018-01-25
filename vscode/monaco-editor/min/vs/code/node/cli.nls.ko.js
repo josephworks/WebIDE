44aa05d2d16bdafba501f32b382cc4b02bd2f161
Joseph Robinson <smartech56@gmail.com>
2018-01-24 20:38:32 -0700

    removed useless files from monaco

--- vscode/monaco-editor/min/vs/code/node/cli.nls.ko.js
+++
@@ -1,7 +0,0 @@
-/*!--------------------------------------------------------
- * Copyright (C) Microsoft Corporation. All rights reserved.
- *--------------------------------------------------------*/
-define("vs/code/node/cli.nls.ko", {
-  "vs/platform/environment/node/argv": ["`--goto` 모드에서 인수는 `FILE(:LINE(:CHARACTER))` 형식이어야 합니다.", "두 파일을 서로 비교합니다.", "마지막 활성 창에 폴더를 추가합니다.", "지정된 줄과 문자 위치에 있는 경로의 파일을 엽니다.", "Code의 새 인스턴스를 강제 적용합니다.", "마지막 활성 창에서 파일 또는 폴더를 강제로 엽니다.", "파일이 닫힐 때 까지 기다린 후 돌아갑니다.", "사용할 로캘(예: en-US 또는 zh-TW)입니다.", "사용자 데이터가 저장되는 디렉터리를 지정합니다(루트로 실행할 경우 유용함).", "버전을 출력합니다.", "사용법을 출력합니다.", "확장의 루트 경로를 설정합니다.", "설치된 확장을 나열합니다.", "#NAME?", "확장을 설치합니다.", "확장을 제거합니다.", "확장에 대해 제안된 API 기능을 사용하도록 설정합니다.", "자세한 정보 표시를 출력합니다(--wait를 의미).", "사용할 로그 수준이며 기본값은 'info'입니다. 허용되는 값은 'critical', 'error', 'warn', 'info', 'debug', 'trace', 'off'입니다.", "프로세스 사용 및 진단 정보를 인쇄합니다.", "'Developer: Startup Performance' 명령을 사용하여 시작합니다.", "시작하는 동안 CPU 프로파일러 실행", "설치된 모든 확장을 사용하지 않도록 설정합니다.", "디버깅 및 확장 프로파일링을 허용합니다. 연결 uri에 대한 개발자 도구를 확인하십시오.", "시작 후 일시 중시된 확장 호스트에서 디버깅 및 확장 프로파일링을 허용합니다. 연결 URL은 개발자 도구를 확인하세요.", "GPU 하드웨어 가속을 사용하지 않도록 설정합니다.", "Uploads logs from current session to a secure endpoint.", "사용법", "옵션", "경로", "To read output from another program, append '-' (e.g. 'echo Hello World | {0} -')", "To read from stdin, append '-' (e.g. 'ps aux | grep code | {0} -')", "옵션", "Extensions Management", "Troubleshooting"]
-});
-//# sourceMappingURL=https://ticino.blob.core.windows.net/sourcemaps/ca023a700f35212071dfe9544272719cbbf80e4c/core/vs\code\node\cli.nls.ko.js.map
--- vscode/monaco-editor/min/vs/code/node/cli.nls.zh-cn.js
+++
@@ -1,7 +0,0 @@
-/*!--------------------------------------------------------
- * Copyright (C) Microsoft Corporation. All rights reserved.
- *--------------------------------------------------------*/
-define("vs/code/node/cli.nls.zh-cn", {
-  "vs/platform/environment/node/argv": ['"--goto" 模式中的参数格式应为 "FILE(:LINE(:CHARACTER))"。', "将两个文件相互比较。", "将文件夹添加到最后一个活动窗口。", "打开路径下的文件并定位到特定行和特定列。", "强制创建一个新的 Code 实例。", "在上一活动窗口中强制打开文件或文件夹。", "等文件关闭后再返回。", "要使用的区域设置(例如 en-US 或 zh-TW)。", "指定存放用户数据的目录。此目录在以 root 身份运行时十分有用。", "打印版本。", "打印使用情况。", "设置扩展的根路径。", "列出已安装的扩展。", "使用 --list-extension 时，显示已安装扩展的版本。", "安装扩展。", "卸载扩展。", "启用扩展程序实验性 api 功能。", "打印详细输出(隐含 --wait 参数)。", '使用的日志级别。默认值为 "info"。允许的值为 "critical" (关键)、"error" (错误)、"warn" (警告)、"info" (信息)、"debug" (调试)、"trace" (跟踪) 和 "off" (关闭)。', "打印进程使用情况和诊断信息。", '通过启用 "Developer: Startup Performance" 命令开始。', "启动期间运行 CPU 探查器", "禁用所有已安装的扩展。", "允许进行扩展的调试与分析。检查开发人员工具可获取连接 URI。", "允许在扩展主机在启动后暂停时进行扩展的调试与分析。检查开发人员工具可获取连接 URI。", "禁用 GPU 硬件加速。", "Uploads logs from current session to a secure endpoint.", "使用情况", "选项", "路径", "To read output from another program, append '-' (e.g. 'echo Hello World | {0} -')", "To read from stdin, append '-' (e.g. 'ps aux | grep code | {0} -')", "选项", "Extensions Management", "Troubleshooting"]
-});
-//# sourceMappingURL=https://ticino.blob.core.windows.net/sourcemaps/ca023a700f35212071dfe9544272719cbbf80e4c/core/vs\code\node\cli.nls.zh-cn.js.map
