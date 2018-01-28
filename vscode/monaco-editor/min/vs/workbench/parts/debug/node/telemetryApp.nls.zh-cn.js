598d007597d38acc0cf03553ce332b98372cec47
Joseph Robinson <smartech56@gmail.com>
2018-01-27 19:50:09 -0700

    removed node

--- vscode/monaco-editor/min/vs/workbench/parts/debug/node/telemetryApp.nls.zh-cn.js
+++
@@ -1,7 +0,0 @@
-/*!--------------------------------------------------------
- * Copyright (C) Microsoft Corporation. All rights reserved.
- *--------------------------------------------------------*/
-define("vs/workbench/parts/debug/node/telemetryApp.nls.zh-cn", {
-  "vs/base/node/processes": ["无法对 UNC 驱动器执行 shell 命令。"]
-});
-//# sourceMappingURL=https://ticino.blob.core.windows.net/sourcemaps/ca023a700f35212071dfe9544272719cbbf80e4c/core/vs\workbench\parts\debug\node\telemetryApp.nls.zh-cn.js.map
